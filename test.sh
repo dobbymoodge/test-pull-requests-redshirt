6ee9dd4692ce2df6cd66ab60f5bb80070644beed
John Lamb <jolamb@redhat.com>
2017-06-22 11:00:03 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo "This test will pass"
+echo "also, get ready for 15 periods over 15 seconds!!! WOOHOO."
+for ii in $(seq 15)
+do
+  sleep 1
+  echo -n '.'
+done
+echo ''
 exit 0
